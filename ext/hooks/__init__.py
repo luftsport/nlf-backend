324cdf4ec52d72e27a0136fb5668f672c22d0c7e
Einar Huseby <einar.huseby@gmail.com>
2019-11-21 15:17:22 +0100

    Delete in collection by keyword hook

--- ext/hooks/__init__.py
+++ ext/hooks/__init__.py
@@ -1,4 +1,4 @@
 
-__all__ = ['fallskjerm.py', 'users', 'help', 'content', 'aircrafts']
+__all__ = ['fallskjerm.py', 'users', 'help', 'content', 'aircrafts', 'e5x']
 
-from . import fallskjerm, motorfly, users, help, content, aircrafts
+from . import fallskjerm, motorfly, users, help, content, aircrafts, e5x
