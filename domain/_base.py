8fa844208fd31ce4f2069efec17aa1e303a34ac5
Einar Huseby <einar.huseby@gmail.com>
2019-11-26 00:17:21 +0100

    Acl schema updates

--- domain/_base.py
+++ domain/_base.py
@@ -7,83 +7,104 @@
 
 """
 
-workflow_schema = {'type': 'dict',
-                   'readonly': True,
-                   }
+workflow_schema = {
+    'type': 'dict',
+    'readonly': True,
+}
 
-watchers_schema = {'type': 'list',
-                   'default': [],
-                   'readonly': True
-                   }
+watchers_schema = {
+    'type': 'list',
+    'default': [],
+    'readonly': True
+}
 
-comments_schema = {'type': 'list',
-                   'default': [],
-                   'readonly': True,
-                   'schema': {
-                       'type': 'dict',
-                       'schema': {
-                           'date': {'type': 'datetime'},
-                           'user': {'type': 'integer'},
-                           'comment': {'type': 'string'}
-                       }
-                   }
-                   }
+comments_schema = {
+    'type': 'list',
+    'default': [],
+    'readonly': True,
+    'schema': {
+        'type': 'dict',
+        'schema': {
+            'date': {'type': 'datetime'},
+            'user': {'type': 'integer'},
+            'comment': {'type': 'string'}
+        }
+    }
+}
 
-ask_schema = {'type': 'dict',
-              'schema': {'attitude': {'type': 'integer'},
-                         'skills': {'type': 'integer'},
-                         'knowledge': {'type': 'integer'},
-                         'text': {'type': 'dict'}
-                         },
-              'default': {'attitude': 0,
-                          'skills': 0,
-                          'knowledge': 0,
-                          'text': {}}
-              }
+ask_schema = {
+    'type': 'dict',
+    'schema': {'attitude': {'type': 'integer'},
+               'skills': {'type': 'integer'},
+               'knowledge': {'type': 'integer'},
+               'text': {'type': 'dict'}
+               },
+    'default': {'attitude': 0,
+                'skills': 0,
+                'knowledge': 0,
+                'text': {}
+                }
+}
 
-acl_type_schema = {'type': 'dict',
-                   'schema': {'users': {'type': 'list'},
-                              'roles': {'type': 'list'}}
-                   }
-acl_item_schema = {'type': 'dict',
-                   'readonly': True,
-                   'schema': {'read': {'type': 'dict', 'schema': acl_type_schema},
-                              'write': {'type': 'dict', 'schema': acl_type_schema},
-                              'execute': {'type': 'dict', 'schema': acl_type_schema},
-                              'delete': {'type': 'dict', 'schema': acl_type_schema},
-                              }
-                   }
+acl_role_schema = {
+    'type': 'dict',
+    'schema': {
+        'activity': {'type': 'integer'},
+        'org': {'type': 'integer'},
+        'role': {'type': 'integer'}
+    }
+}
 
-labels_schema = {'type': 'list',
-                 'default': []
-                 }
+acl_type_schema = {
+    'type': 'dict',
+    'schema': {'users': {'type': 'list'},
+               'roles': {'type': 'list'}}
+}
 
-geo_schema = {'geo': {'type': 'point'},
-              'geo_class': {'type': 'string'},
-              'geo_importance': {'type': 'float'},
-              'geo_place_id': {'type': 'integer'},
-              'geo_type': {'type': 'string'}
-              }
+acl_item_schema = {
+    'type': 'dict',
+    'readonly': True,
+    'schema': {'read': {'type': 'dict', 'schema': acl_type_schema},
+               'write': {'type': 'dict', 'schema': acl_type_schema},
+               'execute': {'type': 'dict', 'schema': acl_type_schema},
+               'delete': {'type': 'dict', 'schema': acl_type_schema},
+               }
+}
 
-location_schema = {'type': 'dict',
-                   'schema': {
-                       'street': {'type': 'string'},
-                       'zip': {'type': 'string'},
-                       'city': {'type': 'string'},
-                       'country': {'type': 'string'}
-                   }
-                   }
+labels_schema = {
+    'type': 'list',
+    'default': []
+}
 
-location_geo_schema = {'type': 'dict',
-                       'schema': {
-                           'street': {'type': 'string'},
-                           'zip': {'type': 'string'},
-                           'city': {'type': 'string'},
-                           'country': {'type': 'string'},
-                           'geo': {'type': 'point'},
-                           'geo_class': {'type': 'string'},
-                           'geo_importance': {'type': 'float'},
-                           'geo_place_id': {'type': 'integer'},
-                           'geo_type': {'type': 'string'}
-                       }
-                       }
+geo_schema = {
+    'geo': {'type': 'point'},
+    'geo_class': {'type': 'string'},
+    'geo_importance': {'type': 'float'},
+    'geo_place_id': {'type': 'integer'},
+    'geo_type': {'type': 'string'}
+}
+
+location_schema = {
+    'type': 'dict',
+    'schema': {
+        'street': {'type': 'string'},
+        'zip': {'type': 'string'},
+        'city': {'type': 'string'},
+        'country': {'type': 'string'}
+    }
+}
+
+location_geo_schema = {
+    'type': 'dict',
+    'schema': {
+        'street': {'type': 'string'},
+        'zip': {'type': 'string'},
+        'city': {'type': 'string'},
+        'country': {'type': 'string'},
+        'geo': {'type': 'point'},
+        'geo_class': {'type': 'string'},
+        'geo_importance': {'type': 'float'},
+        'geo_place_id': {'type': 'integer'},
+        'geo_type': {'type': 'string'}
+    }
+}
