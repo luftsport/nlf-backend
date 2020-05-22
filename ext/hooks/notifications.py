e07520a4e53e9d4d6c16e32b201515a96204c8e4
Einar <einar.huseby@gmail.com>
2020-05-22 17:50:40 +0200

    Clleanup

--- ext/hooks/notifications.py
+++ ext/hooks/notifications.py
@@ -7,21 +7,13 @@ from eve.methods.get import getitem_internal
 from ext.auth.acl import has_permission
 
 
-# from ext.app.decorators import require_token # Already authenticated
-
-
 def before_get(request, lookup):
     """Make sure only recepient can read own messages"""
     lookup['recepient'] = app.globals['user_id']
 
 
 def before_insert(items):
-    print('Before POST content to database')
-    print(items)
-    # payload = json.loads(response.get_data().decode('UTF-8'))
     for document in items:
-        # update document 'userid' field according to my_arg
-        # value. replace with custom logic.
         document['owner'] = app.globals.get('user_id')
 
 
@@ -38,15 +30,15 @@ def before_aggregation(endpoint, pipeline):
     If ORS is closed, none?"""
 
     if endpoint == 'notifications_events':
-        print(endpoint, pipeline)
-
         _id = pipeline[0].get('$match', {}).get('event_from_id')
+
         resource = pipeline[0].get('$match', {}).get('event_from')
         item, _date, etag, status = getitem_internal(resource, **{'_id': _id})
 
-        print(item.get('acl'))
-
-        # ors? closed? Hwo has access??
-        print('PERMISSIONS?', has_permission(item.get('acl'), 'read'))
-
-        # print(status, response)
+        if (
+                (item.get('workflow', {}).get('state', 'closed') == 'closed' and item.get('acl_user', {}).get('x',
+                                                                                                              False) is False)
+                or
+                (item.get('acl_user', {}).get('r', False) is False)
+        ):
+            eve_helper.eve_abort(403, 'No access')
