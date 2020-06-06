407673541061829b9c147c9f10d1b8967af39ba6
Einar <einar.huseby@gmail.com>
2020-06-06 20:19:40 +0200

    Missing ors blueprint

---
+++ blueprints/ors.py
@@ -0,0 +1,48 @@
+from flask import Blueprint, current_app as app
+from ext.app.decorators import require_token
+from ext.app.eve_helper import eve_abort, eve_response
+
+from eve.methods.get import get_internal
+
+UserORS = Blueprint('Users ORSes', __name__, )
+
+
+@UserORS.route("/<regex('(fallskjerm|motorfly)'):collection>", methods=['GET'])
+@require_token()
+def my_orses(collection):
+    data = []
+    status = 404
+    try:
+        person_id = app.globals.get('user_id')
+        collection = '{}_observations'.format(collection)
+
+        lookup = {
+            '$or':
+                [
+                    {'involved.id': person_id},
+                    {'organization.hfl.id': person_id},
+                    {'organization.hm.id': person_id},
+                    {'organization.hl.id': person_id},
+                    {'organization.pilot.id': person_id},
+                    {'aircrafts.crew.person': person_id}
+                ],
+            'workflow.state': {'$in': ['closed']}
+        }
+
+        response, _, _, status, _ = get_internal(collection, **lookup)
+        if status == 200:
+            data = [
+                {
+                    'id': x['id'],
+                    'title': '/'.join(x['tags']),
+                    'discipline': x['discipline'],
+                    'state': x['workflow']['state'],
+                    'type': x['type'],
+                    'when': x.get('when', None),
+                    'rating': x.get('rating', {})
+                } for x in response.get('_items', [])
+            ]
+    except Exception as e:
+        pass
+    
+    return eve_response(data, status)
