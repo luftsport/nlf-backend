32cce83e7eb6605b467c635260abf7822e1eb328
Einar <einar.huseby@gmail.com>
2020-08-06 11:51:28 +0200

    added file hooks file

---
+++ ext/hooks/files.py
@@ -0,0 +1,26 @@
+from ext.auth.acl import has_permission
+from ext.app.eve_helper import eve_abort
+
+
+def _after_fetched_item(response):
+    try:
+
+        response = _anon(response)
+
+    except:
+        return eve_abort(500, 'Anon file aborted')
+
+
+def _after_fetched_list(response):
+    try:
+        for key, item in enumerate(response.get('_items', [])):
+            response['_items'][key] = _anon(item)
+    except:
+        return eve_abort(500, 'Anon file aborted')
+
+
+def _anon(file_dict):
+    if has_permission(file_dict['ref_id'], 'write', file_dict['ref']) is False:
+        file_dict.pop('owner', None)
+
+    return file_dict
