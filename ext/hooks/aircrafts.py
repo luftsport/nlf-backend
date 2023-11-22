4fed006b2f81e607c100f1df71904c39a82ba8b1
Einar <einar.huseby@gmail.com>
2023-11-22 13:59:48 +0100

    Cleanup

--- ext/hooks/aircrafts.py
+++ ext/hooks/aircrafts.py
@@ -6,7 +6,7 @@
 """
 
 from ext.app.decorators import *
-
+from flask import current_app as app, redirect, abort, Response
 def on_insert(items):
     for key, item in enumerate(items):
         items[key]['updated_by'] = int(g.user_id)
@@ -14,3 +14,17 @@ def on_insert(items):
 
 def on_update(item, original):
     item['updated_by'] = int(g.user_id)
+
+def after_GET(request, payload):
+    print('##################################################->')
+    #print('Dadada')
+    headers = {
+        'Location': '/ABRACADABRA'
+    }
+    abort(
+        Response(
+            response=None,
+            status=301,
+            headers=headers
+        )
+    )
